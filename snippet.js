212b7ad947ac7beb3faff0f7dd5b1c7010ee51f5
Atul Varma <varmaa@gmail.com>
2014-06-22 15:31:34 -0400

    fade in dotted outline and arrow tooltip.

--- snippet.js
+++ snippet.js
@@ -1,6 +1,21 @@
 var Snippet = (function() {
+  function startRemixing() {
+    // TODO: Fade out snippet text, fade in next step.
+    $('.body-frame').addClass('selected');
+    setTimeout(function() {
+      $('.body-frame .arrow-box')
+        .addClass('selected')
+        .one('transitionend', function() {
+          console.log('TODO');
+        });
+    }, 1000);
+  }
+
   function start() {
-    console.log('hi');
+    $("#begin").click(function() {
+      startRemixing();
+      return false;
+    });
   };
 
   return {
